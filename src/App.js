cbea4f9c572eb4edc546cd011a903ba2b8ea42a3
Marajul-BS23 <marajul.islam@brainstation-23.com>
2022-01-24 12:35:27 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Movies from "./components/movies.component";
+import Navbar from "./components/navbar.component";
+
+class App extends React.Component {
+    render() {
+        return (
+            <>
+                <Navbar />
+                <br />
+                <Movies />
+            </>
+        );
+    }
+}
+
+export default App;
